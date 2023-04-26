ddb9c332537c784da6543804fca11f9fb4f4e510
King Yeung Lee <kingyeung129@hotmail.com>
2023-04-26 17:50:45 +0800

    Added R Script for Shapiro Wilk normality testing

--- normality_test.R
+++ normality_test.R
@@ -3,35 +3,57 @@ library(conflicted)
 library(tidyverse)
 library(ggplot2)
 library(readxl)
+library(chron)
+library(lubridate)
 
 conflicts_prefer(dplyr::filter)
 
 # Read_CSV On Desktop
 filePath <- "F 200 Freestyle.xlsx"
-swimming_data <- read_excel(filePath, sheet="Competitors 2019-2023")
-# print(swimming_data)
+swiming_data_2019to2023<- read_excel(filePath, sheet="Competitors 2019-2023")
+swiming_data_2022to2023 <- read_excel(filePath, sheet="Competitors 2022-2023")
+swiming_data_2023 <- read_excel(filePath, sheet="Competitors 2023")
 
 # Filter Unqiue Swimmers, Loop through each swimmer and perform Shapiro Wilk's Test
-UniqueSwimmers <- unique(swimming_data$full_name_computed)
+UniqueSwimmers <- unique(swiming_data_2019to2023$full_name_computed)
 for (i in 1:length(UniqueSwimmers)) {
-  Swimmer <- filter(swimming_data, full_name_computed == UniqueSwimmers[i])
-  Swimmer$swim_time <-as.numeric(Swimmer$swim_time)
   print(UniqueSwimmers[i])
-  print(shapiro.test(Swimmer$swim_time))
+  dataset_used = "2023"
+  Swimmer <- filter(swiming_data_2023, full_name_computed == UniqueSwimmers[i])
+
+  # Conditional Checks for Count of Dataframe Object. If less than 3, escalate to next dataset.
+  if (nrow(Swimmer) < 3) {
+    dataset_used = "2022-2023"
+    Swimmer <- filter(swiming_data_2022to2023, full_name_computed == UniqueSwimmers[i])
+  }
+
+  if (nrow(Swimmer) < 3) {
+    dataset_used = "2019-2023"
+    Swimmer <- filter(swiming_data_2019to2023, full_name_computed == UniqueSwimmers[i])
+  }
+
+  Swimmer$swim_time <-period_to_seconds(ms(Swimmer$swim_time))
+  shap_test = shapiro.test(Swimmer$swim_time)
+
+  if (shap_test$p.value <  0.05) {
+    dataset_used = "2022-2023"
+    print("Shapiro-wilk test lesser than 0.05, trying 2022 to 2023 dataset")
+    Swimmer <- filter(swiming_data_2022to2023, full_name_computed == UniqueSwimmers[i])
+    Swimmer$swim_time <-period_to_seconds(ms(Swimmer$swim_time))
+    shap_test = shapiro.test(Swimmer$swim_time)
+    print(shap_test$p.value)
+  }
+
+  if (shap_test$p.value <  0.05) {
+    dataset_used = "2019-2023"
+    print("Shapiro-wilk test lesser than 0.05, trying 2019 to 2023 dataset")
+    Swimmer <- filter(swiming_data_2019to2023, full_name_computed == UniqueSwimmers[i])
+    Swimmer$swim_time <-period_to_seconds(ms(Swimmer$swim_time))
+    shap_test = shapiro.test(Swimmer$swim_time)
+    print(shap_test$p.value)
+
+  }
+
+  print(paste("Final Shapiro wilk p-value:", shap_test$p.value, ". Dataset Used:", dataset_used))
+
 }
-
-'
-# Graph Plotting and Individual Swimmer Analysis
-Swimmer <- filter(swimming_data, full_name_computed == "CREWS, Kaikea")
-Swimmer$swim_time <-as.numeric(Swimmer$swim_time)  
-shapiro.test(Swimmer$swim_time)
-
-
-ggplot(Swimmer, aes(x=swim_time)) + geom_histogram()
-qplot(sample = swim_time, data = Swimmer)
-cat("Mean is:", mean(Swimmer$swim_time), "\n")
-cat("Stdev is:", sd(Swimmer$swim_time), "\n")
-cat("No of Observations:", length(Swimmer$swim_time))
-plot(density(Swimmer$swim_time))
-qqnorm(Swimmer$swim_time);qqline(Swimmer$swim_time, col = 2)
-'
